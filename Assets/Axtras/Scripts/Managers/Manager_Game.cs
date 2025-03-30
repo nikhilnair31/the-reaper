448e9b2423fd93807c699af667d25c9b3b0ee66f
Nikhil Nair <niknair31898@gmail.com>
2025-03-30 17:18:03 -0400

    Added in corpse spawning

--- Assets/Axtras/Scripts/Managers/Manager_Game.cs
+++ Assets/Axtras/Scripts/Managers/Manager_Game.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using System.Collections.Generic;
+using GogoGaga.OptimizedRopesAndCables;
+using System.Linq;
 
 public class Manager_Game : MonoBehaviour 
 {
@@ -8,6 +11,11 @@ public class Manager_Game : MonoBehaviour
     private GameObject playerGO;
     private GameObject mainCameraGO;
     private Data_General generalData = new();
+
+    [Header("Gen Settings")]
+    [SerializeField] private List<Transform> hangingPointsTransformList;
+    [SerializeField] private GameObject corpsePrefab;
+    [SerializeField] private int maxCorpses = 3;
 
     [Header("Run Settings")]
     [SerializeField] private int run = 0;
@@ -37,6 +45,19 @@ public class Manager_Game : MonoBehaviour
             playerGO = GameObject.FindGameObjectWithTag("Player");
         if (mainCameraGO == null)   
             mainCameraGO = Camera.main.gameObject;
+
+        // Initialize the hangin points if not
+        if (hangingPointsTransformList.Count == 0) {
+            var tree = GameObject.FindGameObjectWithTag("Tree");
+            if (tree != null) {
+                var allChilds = tree.GetComponentsInChildren<Transform>();
+                hangingPointsTransformList = allChilds.Skip(1).ToList();
+                Debug.Log($"Assigned {hangingPointsTransformList.Count} hanging points!");
+            }
+            else {
+                Debug.LogError($"Couldn't find the tree!");
+            }
+        }
     }
     private void DataLoad() {
         // Load general data from save file
@@ -50,8 +71,25 @@ public class Manager_Game : MonoBehaviour
         SetScore(generalData.scoreCnt);
     }
 
+    #region Run Related
     public void StartRun() {
         Debug.Log("Game Run Started");
+
+        // Clear corpses
+        var corpses = GameObject.FindGameObjectsWithTag("Corpse");
+        foreach (var corpse in corpses) {
+            Destroy(corpse.transform.parent.gameObject);
+        }
+
+        // Spawn corpses
+        var pointToPickFrom = hangingPointsTransformList;
+        for (int i = 0; i < maxCorpses; i++) {
+            var corpse = Instantiate(corpsePrefab);
+            var rope = corpse.transform.GetComponentInChildren<Rope>();
+            var randIndex = Random.Range(0, pointToPickFrom.Count);
+            var randHangPoint = pointToPickFrom[randIndex];
+            rope.startPoint.position = randHangPoint.position;
+        }
 
         // Prewarm physics steps
         for (int i = 0; i < 100; i++) {
@@ -83,7 +121,9 @@ public class Manager_Game : MonoBehaviour
         // Set time to frozen
         Time.timeScale = 0f;
     }
-    
+    #endregion
+
+    #region Score Related
     public void IncScore() {
         Debug.Log($"IncScore | Score: {score} | ScoreInc: {scoreInc}");
         score += scoreInc;
@@ -92,7 +132,9 @@ public class Manager_Game : MonoBehaviour
         Debug.Log($"DecScore | Score: {score} | ScoreInc: {scoreInc}");
         score -= scoreInc;
     }
+    #endregion
 
+    #region Save Load Related
     public Data_General GetGeneral() {
         return generalData;
     }
@@ -103,4 +145,5 @@ public class Manager_Game : MonoBehaviour
     private void SetScore(int scoreval) {
         score = scoreval;
     }
+    #endregion
 }
